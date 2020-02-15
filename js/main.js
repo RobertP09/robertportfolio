823a3e34ce00ec40af850000c14ff506951c6f4e
Robert <robgaray9111@yahoo.com>
2020-02-15 13:21:09 -0800

    Testing another email method

--- js/main.js
+++ js/main.js
@@ -6,12 +6,6 @@ document.addEventListener('DOMContentLoaded', function(){
         addActive(nav);
     });
 
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate the contact number value
-        this.contact_number.value = Math.random() * 100000 | 0;
-        emailjs.sendForm('contact_service', 'contact_template', this);
-    });
 });
 
  const addActive = (element) => {
@@ -22,4 +16,3 @@ document.addEventListener('DOMContentLoaded', function(){
         this.classList.remove("header__link--active");
     });
 };
-
