68ca08ab7e7ff95895ade9860bdaa1f943cbb402
Robert <robgaray9111@yahoo.com>
2020-02-18 12:21:34 -0800

    Finished Mobile

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
-
+// Elements
 const navLinks = document.querySelectorAll(".header__link");
-navLinks[0].classList.toggle("header__link--active");
 const mobileMenu = document.querySelector(".hamburger");
 const linkMenu = document.querySelector(".mHeader__menu");
+const mNavLinks = document.querySelectorAll(".mHeader__link");
 
+// Created a default highlight
+navLinks[0].classList.toggle("header__link--active");
+
+// Functions
  const addActive = (element) => {
     element.addEventListener('mouseover',() => {
         element.classList.add("header__link--active");
     });
-    element.addEventListener('mouseout', () => {
-        this.classList.remove("header__link--active");
+    element.addEventListener('mouseout',() => {
+        element.classList.remove("header__link--active");
     });
 };
 
+const closeMobileMenu = (element) => {
+    element.addEventListener('click', () => {
+        linkMenu.classList.add("mHeader__menu--closed");
+        mobileMenu.classList.remove("is-active");
+    })
+};
+
+/* Events */
 navLinks.forEach(nav => {
     addActive(nav);
+});
+mNavLinks.forEach(nav => {
+    closeMobileMenu(nav);
 });
 
 mobileMenu.addEventListener('click', function() {
@@ -27,6 +42,3 @@ mobileMenu.addEventListener('click', function() {
         linkMenu.classList.add("mHeader__menu--closed");
     }
 });
-
-//add on click for each element  -- foreach loop add event
-//once clicked removed is-active
