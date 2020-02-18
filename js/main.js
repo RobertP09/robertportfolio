edcc1df5a2ecfb86427ad0de2d4fa4146cc3c4f4
Robert <robgaray9111@yahoo.com>
2020-02-18 00:52:44 -0800

    Add mobile menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 
 const navLinks = document.querySelectorAll(".header__link");
 navLinks[0].classList.toggle("header__link--active");
+const mobileMenu = document.querySelector(".hamburger");
+const linkMenu = document.querySelector(".mHeader__menu");
 
  const addActive = (element) => {
-    element.addEventListener('mouseover', function() {
+    element.addEventListener('mouseover',() => {
         element.classList.add("header__link--active");
     });
-    element.addEventListener('mouseout', function() {
+    element.addEventListener('mouseout', () => {
         this.classList.remove("header__link--active");
     });
 };
@@ -14,3 +16,17 @@ navLinks[0].classList.toggle("header__link--active");
 navLinks.forEach(nav => {
     addActive(nav);
 });
+
+mobileMenu.addEventListener('click', function() {
+    let active = mobileMenu.classList.contains("is-active");
+    if(!active){
+        mobileMenu.classList.add("is-active");
+        linkMenu.classList.remove("mHeader__menu--closed");
+    } else {
+        mobileMenu.classList.remove("is-active");
+        linkMenu.classList.add("mHeader__menu--closed");
+    }
+});
+
+//add on click for each element  -- foreach loop add event
+//once clicked removed is-active
